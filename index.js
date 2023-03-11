302909aa80f008b64875e1379c5676d226572129
AjPitaji <89189452+AjPitaji@users.noreply.github.com>
2023-03-11 21:39:27 +0530

    Update index.js

--- index.js
+++ index.js
@@ -10,6 +10,15 @@ app.set('view engine', 'ejs');
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 mongoose.connect(process.env.mongoSt)
+const connectDB = async ()=> {
+try {
+const conn = await mongoose.connect (process.env.mongoSt);
+console.log(`MongoDB Connected: ${conn.connection.host}`);
+catch (error)
+console.log(error);
+process.exit(1);|
+}
+}
 
 const MemberSchema = new Schema(
   {
@@ -106,7 +115,8 @@ app.route("/UpdateMember")
 //Delete Update performed
 
 
-
+connectDB().then(() => {
 app.listen(process.env.PORT||3000, function() {
   console.log("Server started on port 3000");
+});
 });
